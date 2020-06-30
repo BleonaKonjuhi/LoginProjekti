4b272febb6cce689e29cedb8e7fc0713789c7a1c
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-06-30 13:31:01 +0200

    Add files via upload

---
+++ CPanelController.java
@@ -0,0 +1,21 @@
+package login;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author amir
+ */
+public class CPanelController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }      
+}
